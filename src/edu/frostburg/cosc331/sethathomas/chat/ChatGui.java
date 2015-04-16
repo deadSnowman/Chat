b56f655695d52f760599d4269fb419ed311ca449
Seth Thomas <romanronin92@mail.com>
2015-04-16 00:22:35 -0400

    Use system look and feel

--- src/edu/frostburg/cosc331/sethathomas/chat/ChatGui.java
+++ src/edu/frostburg/cosc331/sethathomas/chat/ChatGui.java
@@ -6,6 +6,7 @@
 package edu.frostburg.cosc331.sethathomas.chat;
 
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 import javax.swing.text.DefaultCaret;
 
 /**
@@ -184,26 +185,12 @@ public class ChatGui extends javax.swing.JFrame {
    * @param args the command line arguments
    */
   public static void main(String args[]) {
-    /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-     */
+    
+    // System look and feel
     try {
-      for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-	if ("Nimbus".equals(info.getName())) {
-	  javax.swing.UIManager.setLookAndFeel(info.getClassName());
-	  break;
-	}
-      }
-    } catch (ClassNotFoundException ex) {
-      java.util.logging.Logger.getLogger(ChatGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (InstantiationException ex) {
-      java.util.logging.Logger.getLogger(ChatGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-      java.util.logging.Logger.getLogger(ChatGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-      java.util.logging.Logger.getLogger(ChatGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+      javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
         //</editor-fold>
 
